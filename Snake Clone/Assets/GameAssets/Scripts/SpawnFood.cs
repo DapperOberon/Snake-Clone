84597d98c330fa62600fc1872eafdf9129bdba73
Cameron Smith <camerosmit@yahoo.com>
2018-02-15 18:54:37 -0800

    Added a highscore, fixed food spawning on snake tail, fixed food not reseting after death.

--- Snake Clone/Assets/GameAssets/Scripts/SpawnFood.cs
+++ Snake Clone/Assets/GameAssets/Scripts/SpawnFood.cs
@@ -4,27 +4,66 @@ using UnityEngine;
 
 public class SpawnFood : MonoBehaviour {
 
+	public static SpawnFood instance = null;
+
 	public GameObject _foodPrefab;
 	public Transform _foodParent;
 	public int _startTimer;
-	public int _respawnTime;
+	public float _respawnTime;
 
 	public Transform _borderTop;
 	public Transform _borderRight;
 	public Transform _borderBottom;
 	public Transform _borderLeft;
 
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+	}
+
 	// Use this for initialization
-	void Start () {
+	private void Start () {
 		InvokeRepeating("Spawn", _startTimer, _respawnTime);
 	}
 
 	private void Spawn()
 	{
+		Debug.Log("Generating food pos...");
+
 		int x = (int)Random.Range(_borderLeft.position.x, _borderRight.position.x);
 		int y = (int)Random.Range(_borderBottom.position.y, _borderTop.position.y);
+		Vector3 pos = new Vector3(x, y, 0);
 
-		GameObject g = (GameObject)Instantiate(_foodPrefab, new Vector2(x, y), Quaternion.identity);
+		Transform[] snakePos = Snake.instance.tail.ToArray();
+
+		GameObject g = (GameObject)Instantiate(_foodPrefab, pos, Quaternion.identity);
 		g.transform.parent = _foodParent;
+
+		for(int i = 0; i < snakePos.Length; i++)
+		{
+			if (snakePos[i].position.Equals(pos))
+			{
+				Debug.LogWarning("Snake and food in same pos, destroying food...");
+				Destroy(g);
+			}
+		}
+	}
+
+	public void ResetFood()
+	{
+		// Reset the score
+		Game.instance.ResetScore();
+
+		foreach (Transform foodGOs in _foodParent)
+		{
+			Destroy(foodGOs.gameObject);
+		}
 	}
 }
